0f346c605c12e14aacca3095d41f558be248e507
Avinash  Dhuriya <160625030+avinash-dhuriyaa@users.noreply.github.com>
2024-04-28 15:38:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+const endDate = "22 september 2024 00:0:00 AM"
+
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input")
+    // const clock = () => {
+
+// }
+
+function clock() {
+    const end = new Date(endDate)
+    const now = new Date()
+    const diff = (end - now) / 1000;
+
+    if (diff < 0) return;
+
+    // convert into days;
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor(diff / 3600) % 24;
+    inputs[2].value = Math.floor(diff / 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
+}
+
+// initial call
+clock()
+
+setInterval(
+    () => {
+        clock()
+    },
+    1000
+)
